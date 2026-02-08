6e6d9fa46b5ae777560edd585ee6fe68e4c9714b
felipe-souza17 <felipesantos.rsouza@gmail.com>
2026-02-07 21:11:49 -0300

    [FIX] Message Filter

--- src/discord/listeners/rpMessageCreate.ts
+++ src/discord/listeners/rpMessageCreate.ts
@@ -4,8 +4,6 @@ import { saveRpEvent } from "../../rp/saveRpEvent";
 
 client.on("messageCreate", async (message) => {
   if (!message.webhookId) return;
-  if (!("parent" in message.channel)) return;
-  if (message.channel.parentId == "995481142608265226") return;
 
   const parsed = parseRpMessage(message);
   if (!parsed) return;
